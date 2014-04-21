73e8effbdc62d1d3c2b3354951e2ac8dcb7d7a18
Brian Lai <b2lai@uwaterloo.ca>
2014-04-21 16:55:48 -0400

    add _.pick

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,1 +1,15 @@
 __author__ = 'brian'
+
+
+def pick(objs, attrs):
+    """From objs, return a list of dicts with only attrs preserved."""
+    buffer = []
+    for obj in objs:
+        dct = {}
+        for attr in attrs:
+            try:
+                dct[attr] = obj[attr]
+            except KeyError:
+                dct[attr] = obj.attr
+            buffer.append(dct)
+    return buffer
